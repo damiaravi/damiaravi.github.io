fa852a27150578831ab617c987e4f3568c1bb886
damiaravi <damia.ravi@devinci.fr>
2017-02-10 14:28:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
+
+function homePageLoading() {
+    url = 'apiUrl';
+    elementID = 'idDeVotreElementHtml';
+    fonctionRequeteApi(url, elementID);
+
+    ...
+}
+
 function output(inp) {
     document.body.appendChild(document.createElement('pre')).innerHTML = inp;
 }
